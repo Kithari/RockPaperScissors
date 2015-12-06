957675ff538c8e641a115088e07617df076b27cd
Michael Gaylor <gaylortm@gmail.com>
2015-12-05 23:02:01 -0600

    Added stat tracking

--- RPS.js
+++ RPS.js
@@ -1,3 +1,10 @@
+var playerWins = 0;
+var playerLosses = 0;
+var gameTies = 0;
+var rockWins = 0;
+var paperWins = 0;
+var scissorsWins = 0;
+var totalGames = 0;
 var rockElement = document.querySelector('#rock');
 var rockClickHandler = function (event) {
   changeText(playAiVsHuman("rock"));
@@ -22,9 +29,55 @@ function changeText(result){
 }
 
 function playAiVsHuman(userChoice){
+  var whoWon = "";
   var computerChoice = compChoice();
   var winner = compare(userChoice, computerChoice);
-  return ("Computer choice was: " + computerChoice + ". " + winner + "<br>");
+  if(winner === 1){
+    playerWins+=1;
+    if(userChoice==="rock"){
+      rockWins+=1;
+    }
+    else if(userChoice==="paper"){
+      paperWins+=1;
+    }
+    else{
+      scissorsWins+=1;
+    }
+    whoWon = "Player wins with choice of: " + userChoice;
+  }
+  else if(winner===2){
+    playerLosses+=1;
+    if(computerChoice==="rock"){
+      rockWins+=1;
+    }
+    else if(computerChoice==="paper"){
+      paperWins+=1;
+    }
+    else{
+      scissorsWins+=1;
+    }
+    whoWon="Computer wins with choice of " + computerChoice;
+  }
+  else{
+    gameTies +=1;
+    whoWon = "Game is a tie."
+  }
+  totalGames+=1;
+  return (whoWon + ". Rock has won: "+ rockWins + ". Paper has won: "+ paperWins +". Scissors has won: "
+              + scissorsWins + ". Player has won: " + playerWins + ". Player has lost: " + playerLosses +
+                  ". Number of ties: " + gameTies + "<br>");
+}
+
+function winCalculator(result, choice){
+  var rockWins = 0;
+  var paperWins = 0;
+  var scissorsWins = 0;
+  if(result===1){
+    if(choice==="rock"){
+      return rockWins+=1
+    }
+
+  }
 }
 
 function compChoice(){
@@ -43,30 +96,33 @@ function compChoice(){
 
 function compare (choice1, choice2){
   if(choice1 === choice2){
-    return "Game is a tie";
+    //3 is being returned as an integer form of "tie" for later use
+    return 3;
   }
   else if(choice1 === "rock"){
     if(choice2 === "paper"){
-      return "Paper wins!";
+      //2 is being returned as an integer form of "Player Loss" for later use
+      return 2;
     }
     else{
-      return "Rock wins!";
+      //1 is being returned as an integer form "Player Win" for later use
+      return 1;
     }
   }
   else if(choice1==="paper"){
     if(choice2==="rock"){
-      return "Paper wins!";
+      return 1;
     }
     else{
-      return "Scissors wins!";
+      return 2;
     }
   }
   else if(choice1==="scissors"){
     if(choice2==="paper"){
-      return "Scissors wins!";
+      return 1;
     }
     else{
-      return "Rock wins!";
+      return 2;
       }
   }
 }
